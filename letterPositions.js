e605c1ca6f2657bb3a2fa01ae2b92a7e8025b821
NadiaPia <206i417@gmail.com>
2022-05-26 02:21:46 +0000

    Implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = function(arr1, arr2) {
+
+  if (eqArrays(arr1, arr2) === false) {
+    console.log(`👹️👹️👹️ Assertion Failed`);
+  } else {
+    console.log(`😍️😍️😍️ Assertion Passed`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    //console.log(i)
+    //console.log(sentence[i])
+    //sentence[i] = h
+    if (results[sentence[i]] !== undefined) {
+      results[sentence[i]].push(i);
+      continue;
+    }
+    results[sentence[i]] = [i]; // results[sentence[i]] value // results -> { sentence[i]: results[sentence[i]]}
+    //console.log(results)
+  }
+  return results;
+};
+//console.log(letterPositions("hello"));
+
+assertArraysEqual(letterPositions("hello").o, [4]);
