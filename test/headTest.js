3469300588bd271bd9d3276ee63e425f750bdf25
NadiaPia <206i417@gmail.com>
2022-06-10 22:45:58 +0000

    Moch and Chai for the head.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,25 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 
-assertEqual(head([5, 3]), 5); //from head.js
-assertEqual(1, 11);
+
+//describe and it are Mocha's functions to register code tests
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello'); 
+  });
+});
+
+/* we replaced this block of test cobe with Mocha's functions to register our tests
+assertEqual(head([5, 3]), 5); //from head.js 
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+*/
+
